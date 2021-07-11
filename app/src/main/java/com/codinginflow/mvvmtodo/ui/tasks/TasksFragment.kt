ce9e1422c93abe7e01677333c2a57243684aa1d9
MartinHalus <halus.martin@gmail.com>
2021-07-11 13:54:32 +0200

    13 of 14 done
    14th start
    https://www.youtube.com/watch?v=z3q943EPP5s

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksFragment.kt
@@ -96,6 +96,11 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
 
                         Snackbar.make(requireView(),event.msg, Snackbar.LENGTH_SHORT).show()
                     }
+                    TasksViewModel.TasksEvent.NavigateToDeleteAllCompletedScreen -> {
+                        val action = TasksFragmentDirections.actionGlobalDeleteAllCompletedDialogFragment()
+                        findNavController().navigate(action)
+
+                    }
                 }.exhaustive
 
             }
@@ -144,6 +149,7 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
                 true
             }
             R.id.action_delete_all_completed_tasks -> {
+                viewModel.onDeleteAllCompletedClick()
 
                 true
             }
